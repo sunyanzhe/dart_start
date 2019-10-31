f68c412ce70359c9ed066cb74d6077fa0c7e4155
unknown <sunyanzhe@360buyad.local>
2019-10-31 15:28:14 +0800

    异常处理

--- 处理异常.dart
+++ 处理异常.dart
@@ -39,3 +39,57 @@ void main2() {
   }
 }
 
+/**
+ * 正如前面的代码所示, 您可以使用on或catch或both
+ * 在需要指定异常类型时使用 当您的异常处理程序需要异常对象时, 请使用catch
+ * 可以指定catch()的一个或两个参数,第一个是抛出的异常,第二个是堆栈跟踪(StackTrace对象)
+ */
+void main3() {
+  try {
+
+  } on Exception catch (e) {
+    print('Exception details: \n $e');
+  } catch (e, s) {
+    print('Exception details: \n $e');
+    print('Stach trace:\n $s');
+  }
+}
+
+//要在捕获中处理异常, 同时允许其继续传播, 请使用rethrow关键字
+void misbehave() {
+  try {
+    dynamic foo = true;
+    print(foo++);   //Runtime error
+  } catch (e) {
+    print('misbehave() partially handled ${e.runtimeType}');
+    rethrow;    //Allow callers to see the exception
+  }
+}
+
+void main() {
+  try {
+    misbehave();
+  } catch (e) {
+    print('main() finished handling ${e.runtimeType}');
+  }
+}
+
+//Finally
+//要确保在抛出异常时运行某些业务代码, 请使用finally子句.
+//如果没有catch子句匹配异常,则在finally子句运行后传播异常
+
+try {
+  breedMoreLlamas();
+} finally {
+  cleanLlamaStalls();
+}
+
+
+//finally子句在所有匹配到的catch子句之后运行
+try {
+  breedMoreLlamas();
+} catch (e) {
+  print('Error: $e');
+} finally {
+  cleanLlamaStalls();
+}
