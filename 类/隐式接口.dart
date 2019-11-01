b6d0ca38199a1303baddf61831632b5c48fe2b37
unknown <sunyanzhe@360buyad.local>
2019-11-01 18:53:58 +0800

    接口

---
+++ 类/隐式接口.dart
@@ -0,0 +1,24 @@
+/**
+ * 每个类都有隐式地定义一个接口
+ * 该接口包含的所有实例成员及其实现的任何接口
+ * 如果想创建一个类A, 它支持B的API而不继承B的实现, 那么类A应该实现B接口
+ */
+
+class Person {
+  final _name;
+  Person(this._name);
+  String greet(String who) => 'Hello, $who. I am $_name';
+}
+
+class Impostor implements Person {
+  get _name => '';
+
+  String greet(String who) => 'Hi $who. Do you know who I am';
+}
+
+
+
+//一个雷实现多个接口
+class Point implements Comparable, Location {
+  
+}
